16f6571fb0d6008a7e30870a61b3cd2e10a461e7
JYH0817 <wnel309@gmail.com>
2021-10-17 23:48:11 +0900

    Backend

---
+++ Backend/crawling_data/views.py
@@ -0,0 +1,20 @@
+# Create your views here.
+from django.shortcuts import render
+from rest_framework.response import Response
+from .models import ReviewData
+from .models import BuildingData
+from rest_framework.views import APIView
+from .serializers import ReviewSerializer
+from .serializers import BuildingSerializer
+from django.shortcuts import render, get_object_or_404
+
+class BuildingInfoAPI(APIView):
+    def get(self, request):
+        queryset = BuildingData.objects.all()
+        serializer = BuildingSerializer(queryset, many=True)
+        return Response(serializer.data)
+class ReviewListAPI(APIView):
+    def get(self, request, slug):
+        queryset = ReviewData.objects.all()
+        serializer = ReviewSerializer(queryset, many=True)
+        return Response(serializer.data)
